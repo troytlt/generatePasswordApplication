c398dc60edee409c0149ab8212a2c23bc8834da5
Le Tran <trihewer@gmail.com>
2020-04-28 11:58:10 -0500

    new version

--- Asset/script.js
+++ Asset/script.js
@@ -1,175 +1,78 @@
-var specialCharacters = [
-'@',
-'%',
-'+',
-'\\',
-'/',
-"'",
-'!',
-'#',
-'$',
-'^',
-'?',
-':',
-',',
-')',
-'(',
-'}',
-'{',
-']',
-'[',
-'~',
-'-',
-'_',
-'.'
-];
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCaseCharacters = [
-    'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-var upperCaseCharacter = [
-    'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-// function to get options from user
-function getOptionsFromUser() {
-    // Ask user for their option
-    var length = parseInt (prompt('How many characters do you want to be included in your password? (please note that password characters must be at least 8)'));
-    if (isNaN(length)===true) {
-        alert('Your password length must be a whole number');
+//Target button and create a listener
+var btn = document.getElementById('createBtn');
+var output = document.getElementById('password');
+function goAhead () {
+    output.textContent = createPass();
+};
+// Create all arrays for special, number, lowercase and uppercase characters
+var speCha = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var numCha = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowCha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upCha =  ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// Define global varibales
+var num, wantSpe, wantNum, wantLow, wantUp;
+// Function to get input from users
+function getInput() {
+    num = parseInt (prompt ('How many characters do you want to include in your password?'));
+    // validate number entered by user
+    if (isNaN(num) === true) {
+        alert('Please enter a valid number');
         return;
     }
-    if (length <8) {
-        alert('Your password length must be at least 8');
+    if (num>100) {
+        alert('You do not need your password that long!');
         return;
     }
-    if (length > 120) {
-        alert('Your password length must be less than 120');
+    if (num<6) {
+        alert('Password must be at least 6 characters');
         return;
     }
-    // Check if user wants to have special C, numbers, Uppercase C or Lowercase included
-    var hasSpecial = confirm('Do you want your passwords to have special characters?');
-    var hasNumbers = confirm("Do you want your passwords to have numbers?");
-    var hasUpperCase = confirm('Do you want your passwords to have Uppcase characters?');
-    var hasLowerCase = confirm('Do you want your Passwords to have lowercase characters');
-    // Check user must input at least one type of characters
-    if (hasSpecial=== false &&
-        hasNumbers===false &&
-        hasUpperCase===false &&
-        hasLowerCase===false) {
-            alert('You must opt for at least one type of character!');
-            return;
-        }
-    var passwordOptions = {
-        length: length,
-        hasSpecial: hasSpecial,
-        hasNumbers: hasNumbers,
-        hasLowerCase: hasLowerCase,
-        hasUpperCase: hasUpperCase
+    wantSpe = confirm ('Do ya want to include special characters in your password?');
+    wantNum = confirm ('Do you want to include numeric characters in your password?');
+    wantLow = confirm ('Do you want to include lowercase characters in your password?');
+    wantUp = confirm ('Do you want to include uppercase characters in your password?');   
+    // Check to see at least one feature must be selected
+    if (wantSpe === false && wantNum===false && wantLow===false && wantUp===false) {
+    alert('At least one feature must be selected');
+    } 
+};
+//Function to get random value from an array
+function rand(arr) {
+    var ranIndex = Math.floor(Math.random()*arr.length);
+    var ranEl = arr[ranIndex];
+    return ranEl;
+}
+//Function to write password
+function createPass() {
+    getInput();
+    // Call a temporary password variable that contain must-have characters
+    var temptPass=[];
+    // Call an array that contains all remaining characters after must-have characters are selected
+    var remainCha=[];
+   if(wantSpe) {
+            temptPass.push(rand(wantSpe));
+            remainCha = remainCha.concat(speCha);
+   };
+    if(wantNum) {
+            temptPass.push(rand(numCha));
+            remainCha = remainCha.concat(numCha);
     };
-    return passwordOptions;
-}
-//function to get a random element from an array
-function getRandom (arr) {
-    var randIndex = Math.floor(Math.random()*arr.length);
-    var randElement = arr[randIndex];
-    return randElement;
-}
-// function to create password with user options
-function generatePassword() {
-    var options = getOptionsFromUser();
-    // define result and push element to it as we progress
-    var result = [];
-    // define the collection of possible characters
-    var possibleCharacters = [];
-    // define characters that must be included
-    var mustHaveCharacters = [];
-    // concat characters of each required type to mustHaveCharacters
-    // push random character in each required type of characters to mustHaveCharacters
-    if (options.hasSpecial) {
-        possibleCharacters = possibleCharacters.concat(specialCharacters);
-        mustHaveCharacters.push(getRandom(specialCharacters));
-    }
-    if (options.hasNumbers) {
-        possibleCharacters = possibleCharacters.concat(numericCharacters);
-        mustHaveCharacters.push(getRandom(numericCharacters));
-    }
-    if (options.hasLowerCase) {
-        possibleCharacters = possibleCharacters.concat(lowerCaseCharacters);
-        mustHaveCharacters.push(getRandom(lowerCaseCharacters));
-    }
-    if (options.hasUpperCase) {
-        possibleCharacters=possibleCharacters.concat(upperCaseCharacter);
-        mustHaveCharacters.push(getRandom(upperCaseCharacter));
-    }
-    //Push mustHaveCharacters to result with a loop within the length required
-    for (var i=0; i<options.length; i++) {
-        result.push(getRandom(possibleCharacters));
-    }
-    for (var i; i<mustHaveCharacters.length; i++) {
-        result[i] = mustHaveCharacters[i];
-    }
-   
-    //Transform the result into a string
-    return result.join("");
-}
-
-var createBtn = document.querySelector('#create');
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector('#password');
-  
-    passwordText.value = password;
-  }
-  
-  // Add event listener to generate button
-  createBtn.addEventListener('click', writePassword);
+    if (wantLow) {
+            temptPass.push(rand(lowCha));
+            remainCha = remainCha.concat(lowCha);
+    };
+    if (wantUp) {
+      
+            temptPass.push(rand(upCha));
+            remainCha = remainCha.concat(upCha);
+    };    
+    // Connect the temptPass and remainCha to ensure the number of password characters is what user wants
+    for (var i=temptPass.length-1; i<num;i++) {
+        temptPass.push(rand(remainCha));
+    };
+    // create final password variable
+    var finalPass= temptPass.join('');
+    return finalPass;
+};
+// Execute the password function once the button is clicked
+btn.addEventListener('click',goAhead);
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # generatePasswordApplication
-// This is an application to create password according to user's requirements
-// Password options must have at least 8 characters
-// Users are asked to include whether password shall have special, uppercase, lowercase or numeric characters
-// Then the application returns the password as the user sees fit
+This is an application that helps generate a secured password as required by user.
+First, user is asked to input a number of characters in his password after clicking the button 'Create Password', then he will be asked whether he wants to include numeric characters or special characters, or lowercase or uppercase characters in his password.
+After the user's input, the output of secured password will be added to the box above the button.
